e73787da80dd5def06c9158d47fdcf1883e5fb51
Chris Jenkinson <chris@chrisjenkinson.org>
2011-02-10 13:29:06 +0000

    Base files for sfKoreroPlugin.

---
+++ config/sfKoreroPluginConfiguration.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * sfKoreroPlugin configuration.
+ * 
+ * @package     sfKoreroPlugin
+ * @subpackage  config
+ * @author      Chris Jenkinson
+ * @version     SVN: $Id: PluginConfiguration.class.php 17207 2009-04-10 15:36:26Z Kris.Wallsmith $
+ */
+class sfKoreroPluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.0-DEV';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
